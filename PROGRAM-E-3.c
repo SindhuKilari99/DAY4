cee043ae0612f57d0c6fdc61f3c9a3eafc257692
SindhuKilari99 <108974193+SindhuKilari99@users.noreply.github.com>
2022-07-14 16:32:05 +0530

    Create PROGRAM-E-3.c

---
+++ PROGRAM-E-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,j,k;
+    printf("enter the values of i,j:");
+    scanf("%d %d",&i,&j);
+  
+
+    k=--i+j++ * i++ -j--; 
+
+   
+    printf("%d",k);
+}
